87c5fce03bd88f01ad3d5a9e7272c4e44b1f76b4
KrasulinN <158701626+KrasulinN@users.noreply.github.com>
2024-10-09 21:07:58 +0300

    Add files via upload

--- src/Controller.php
+++ src/Controller.php
@@ -3,7 +3,12 @@
 namespace Markause\GuessNumber\Controller;
 
 use Markause\GuessNumber\View;
+use Markause\GuessNumber\Game;
 
-function startGame() {
-    View\showStartScreen();
+function startGame($maxNumber, $maxAttempts, $saveToDatabase = false) {
+	if ($saveToDatabase) {
+        \cli\line("Пока что базы данных не работают.");
+    }
+    $game = new Game($maxNumber, $maxAttempts);
+    $game->play();
 }
